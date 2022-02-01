cb89d6186dc59c619e99bd6887d6a414bff4ac21
Geraldo Quende <adolfoquende@hotmail.com>
2022-02-01 11:12:07 +0100

    Restart

--- lib/core/usecases/usecase.dart
+++
@@ -1,6 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:flutter_tdd/core/error/failures.dart';
-
-abstract class Usecase<Type, Input> {
-  Future<Either<Failure, Type>> call(Input input);
-}
